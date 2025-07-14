4c5af561056755b98cc436c6c2e5b81223e7405d
Noel Mathen <noelmathen03@gmail.com>
2025-07-14 10:02:29 +0530

    First commit

---
+++ 1_Basics/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("New line");
+        System.out.print("No New line");
+    }
+}
+
