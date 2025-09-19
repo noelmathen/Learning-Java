b533f9d8e6462f8f871533bfa65e6d8fdd7ec285
noelmathen <noelmathen03@gmail.com>
2025-09-19 11:03:31 +0530

    Classes and Objebcts Done

---
+++ 10_Classes and Objects/Class.java
@@ -0,0 +1,71 @@
+// User function Template for Java
+
+// Design a class as described in the question
+// Code here
+class MyClass{
+    public static void display(){
+        System.out.println("Hello World");
+    }
+}
+
+
+
+
+// User function Template for Java
+
+// design a class User
+// instance variable: name(String)
+// Constructor: no parameter, initialize name to "Default"
+
+// code here
+class User{
+    String name;
+    
+    User(){
+        this.name = "Default";
+    }
+}
+
+
+
+
+
+// User function Template for Java
+
+// design a class with two constructor
+// one default, the other parameterized
+
+// code here
+class User{
+    String name;
+    
+    User(){
+        this.name = "Default";
+    }
+    
+    User(String name){
+        this.name = name;
+    }
+}
+
+
+
+
+
+
+// create a class addition
+// with a static function add
+// this should return the sum of
+// two values(a,b) passed in the parameter
+
+// code here
+class Addition{
+    public static int add(int a, int b){
+        return a + b;
+    }
+}
+
+
+
+
+
